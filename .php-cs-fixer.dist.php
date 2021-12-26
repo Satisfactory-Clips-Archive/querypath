eeac4788340921bf1c72b9ad7806bd63b0cf06c3
SignpostMarv <me@signpostmarv.name>
2021-12-26 14:47:12 +0000

    adding php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* PHP-CS-Fixer Config.
+*
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\CS;
+
+class QueryPathConfig extends ConfigUsedWithStaticAnalysis
+{
+    protected static function RuntimeResolveRules() : array
+    {
+        $rules = parent::RuntimeResolveRules();
+
+        $rules['php_unit_method_casing'] = false;
+        $rules['php_unit_test_case_static_method_calls'] = false;
+
+        return $rules;
+    }
+}
+
+return QueryPathConfig::createWithPaths(...[
+    __FILE__,
+    __DIR__ . '/src/',
+    __DIR__ . '/tests/',
+]);
